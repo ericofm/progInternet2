6c6ac0721be43573f48fc4df2b3f91eca0652ca2
ericofm <67295805+ericofm@users.noreply.github.com>
2020-09-24 11:51:21 -0300

    Update buscaProdutos.js

--- Aula06/buscaProdutos.js
+++ Aula06/buscaProdutos.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection( {
     host : "localhost" , 
     user : "root" ,
-    password : "852123" ,
+    password : "" ,
     database : "api_rest"
 });
 
